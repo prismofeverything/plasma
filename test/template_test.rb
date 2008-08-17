88b2a32c671d281793a62e1f8db29375e072904f
ooxwo <ooxwo@anazeentibot.lan>
2008-08-16 23:48:58 -0700

    added hanoi example (from m4)

--- test/template_test.rb
+++ test/template_test.rb
@@ -1,6 +1,6 @@
 require 'lib/plasma.rb'
 
-template = File.open(File.join(PLASMA_TEST, "template_test.plasma")).read
+template = File.open(File.join(PLASMA_TEST, ARGV[0])).read
 plasma = Plasma::Interpreter::PlasmaGrammarParser.new
 
 env = Plasma::Interpreter::Env.new
